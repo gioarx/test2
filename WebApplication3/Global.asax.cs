0be1c5efdf8d7273029aa64cc50416b5512045dd
Giovanni Arcifa <giovanniarcifa@gmail.com>
2014-09-02 17:57:55 +0200

    test222

--- WebApplication3/Global.asax.cs
+++ WebApplication3/Global.asax.cs
@@ -11,7 +11,7 @@ namespace WebApplication3
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {
+        {//sss
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
